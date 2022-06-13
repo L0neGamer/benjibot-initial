396a3eba3968cc829c109a9a56526248f65d3ff7
L0neGamer <benjaminbwm@gmail.com>
2022-06-13 23:31:13 +0100

    updated tablebot version

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Tablebot
 import Tablebot.Internal.Plugins
 import Tablebot.Internal.Types
 import Tablebot.Plugins.Ping
+import Data.Text
 
 main :: IO ()
-main = runTablebotWithEnv [pingpong, compilePlugin netrunnerPlugin] def
+main = runTablebotWithEnv [pingpong, compilePlugin netrunnerPlugin] (def {botName=pack "Sahasrara"})
